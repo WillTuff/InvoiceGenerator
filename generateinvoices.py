83e296531cecfbbd1f41acee41fbf19cd14470d3
Will Tuff <willtuff@hotmail.com>
2019-06-15 21:46:43 +0100

    Added config file and config variables

--- generateinvoices.py
+++ generateinvoices.py
@@ -1,27 +1,36 @@
 import os
+import datetime
+import ConfigParser
 from tempfile import NamedTemporaryFile
 from InvoiceGenerator.api import Invoice, Item, Client, Provider, Creator, Address
 from InvoiceGenerator.pdf import SimpleInvoice
 
-#choose english as language
+# choose english as language
 os.environ["INVOICE_LANG"] = "en"
 
-accNumber = '00000000'
-sortCode = '000000'
-client = Client('The Inside Story')
-provider = Provider('Totness Web Services, 4 Broadway, Guiseley, Leeds', 
-    bank_account=accNumber, bank_code=sortCode)
-creator = Creator('')
-address = Address('Acme Solutions', address = 'The Hague, Netherlands')
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
 
-#inputs
-itemQuantity = input("How many items would you like to add? ")
+# import info from config.ini
+configFile = "myvars"
+accNumber = config.get(configFile, "account_number")
+sortCode = config.get(configFile, "sort_code")
+address = config.get(configFile, "address")
+
+client = Client('The Inside Story')
+provider = Provider(address,
+                    bank_account=accNumber, bank_code=sortCode)
+creator = Creator('')
+
+invoice = Invoice(client, provider, creator)
+invoice.currency_locale = 'en_UK.UTF-8'
+invoice.currency = u'\u00a3'
+invoice.number = '1'
+
 
 def GenerateInvoice():
     i, x = 0, 1
-    invoice = Invoice(client, provider, creator)
-    invoice.currency_locale = 'en_UK.UTF-8'
-    invoice.currency = u'\u00a3'
+    itemQuantity = input("How many items would you like to add? ")
 
     while i < itemQuantity:
         print('Please enter the details for Item ' + str(x))
@@ -31,11 +40,9 @@ def GenerateInvoice():
         invoice.add_item(Item(Quantity, Price, Description))
         x += 1
         i += 1
-
     pdf = SimpleInvoice(invoice)
     invoiceName = "Invoice001.pdf"
-    pdf.gen(invoiceName, generate_qr_code=True) 
-
+    pdf.gen(invoiceName, generate_qr_code=True)
 
 GenerateInvoice()
 print('Invoice Generated!')
